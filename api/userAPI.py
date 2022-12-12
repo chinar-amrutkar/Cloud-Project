e74b663c7bfa5497513a64c9095e8732a95d7cef
chinar-amrutkar <chinar.amrutkar@gmail.com>
2022-12-12 18:47:56 +0000

    added smart_assist feature

--- api/userAPI.py
+++ api/userAPI.py
@@ -60,8 +60,6 @@ def temperature(city):
         max_temp = max(contents['hourly']['temperature_180m'])
         min_temp = min(contents['hourly']['temperature_180m'])
         current_temp = contents['current_weather']['temperature']
-        print(max_temp)
-        print(min_temp)
         s = f"Today's forecast:\n\nCurrent temperature: {current_temp} \nMax temperature: {max_temp} \nMin temperature: {min_temp}"
         return s, 200
     except Exception as e:
@@ -79,9 +77,30 @@ def wind_speed(city):
         max_wind = max(contents['hourly']['windspeed_180m'])
         min_wind = min(contents['hourly']['windspeed_180m'])
         current_wind = contents['current_weather']['windspeed']
-        print(max_wind)
-        print(min_wind)
         s = f"Today's forecast:\n\nCurrent windspeed: {current_wind} \nMax windspeed: {max_wind} \nMin windspeed: {min_wind}"
+        return s, 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+@userAPI.route('/shouldIGoOut/<city>')
+def smart_assist(city):
+    try:
+        city_info = user_Ref.document(city).get().to_dict()
+        lat = city_info['lat']
+        long = city_info['long']
+        temp = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=windspeed_180m,winddirection_180m&current_weather=true&start_date=2022-12-11&end_date=2022-12-11")
+        contents = json.loads(temp.text)
+        current_wind = contents['current_weather']['windspeed']
+        current_temp = contents['current_weather']['temperature']
+
+        
+        
+        temp_ok = "The temperature is not ideal today, maybe tomorrow!" if current_temp <10 or current_temp > 30 else "Perfect day to go out for an adventure!"
+        wind_ok = "You'll enjoy a nice breeze today!" if current_wind <15 else "Too windy, better stay indoors!"
+
+        
+        s = f"Today's forecast:\n\n{temp_ok} \n{wind_ok}"
         return s, 200
     except Exception as e:
         return f"An Error Occured: {e}"
